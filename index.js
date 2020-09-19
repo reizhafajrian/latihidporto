88426a1092e185e3cd47e75bf2e5747047b27a13
reizha <reizha77@gmail.com>
2020-09-19 15:33:19 +0700

    fix navbar

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const main = () => {
         const menu = document.getElementById('collapse-menus');
         const send = document.getElementById('send');
         const popup = document.getElementById('popup');
+        const menuToggle = document.querySelector('.menu-toggle input');
+        const nav = document.querySelector('.nav-bar');
+        const button = document.querySelector('.button')
+        menuToggle.addEventListener('click', () => {
+            if (nav.style.opacity == "1") {
+                nav.style.opacity = "0";
+                button.style.opacity = "0"
+            } else {
+                nav.style.opacity = "1";
+                button.style.opacity = "1"
+            }
+        });
         collapse.onclick = () => {
             if (menu.style.display == 'block') {
                 menu.style.display = 'none'
